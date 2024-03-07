f508eab4ac6aaf353f1def93db395ee12675fa90
labscloud0101 <162542104+labscloud0101@users.noreply.github.com>
2024-03-07 15:14:29 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,52 +10,36 @@ if ! [ -x "$(command -v npm)" ]; then
   exit 1
 fi
 
-PS3='Please select an option from the above choices: '
-options=("Serverless Attack Lab"
-        "Quit")
-
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Serverless Attack Lab")
-            echo "Serverless Attack Lab"
-            sed -i'' -e "s/myrandombucket/$(uuidgen | tr '[:upper:]' '[:lower:]')/g" serverless.yml
-            sed -i 's/us-west-2/us-east-1/g' serverless.yml
-            echo '[INSTALL] Found Python3'
-            python3 -m pip -V
-            if [ $? -eq 0 ]; then
-            echo '[INSTALL] Found pip'
-            if [[ $unamestr == 'Darwin' ]]; then
-                python3 -m pip install --no-cache-dir --upgrade pip
-            else
-                python3 -m pip install --no-cache-dir --upgrade pip --user
-            fi
-            else
-            echo '[ERROR] python3-pip not installed'
-            exit 1
-            fi
-            echo '[INSTALL] Using python virtualenv'
-            rm -rf ./venv
-            python3 -m venv ./venv
-            if [ $? -eq 0 ]; then
-                echo '[INSTALL] Activating virtualenv'
-                source venv/bin/activate
-                pip install --upgrade pip wheel
-            else
-                echo '[ERROR] Failed to create virtualenv'
-                exit 1
-            fi
-            echo '[INSTALL] Installing Requirements'
-            pip install --no-cache-dir --use-deprecated=legacy-resolver -r requirements.txt
-            npm install fast-xml-parser
-            npm install -g serverless
-            sls plugin install -n serverless-python-requirements && sls plugin install -n serverless-s3-deploy && sls plugin install -n serverless-wsgi
-            sls deploy
-            break
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+echo "Serverless Attack Lab"
+sed -i'' -e "s/myrandombucket/$(uuidgen | tr '[:upper:]' '[:lower:]')/g" serverless.yml
+sed -i 's/us-west-2/us-east-1/g' serverless.yml
+echo '[INSTALL] Found Python3'
+python3 -m pip -V
+if [ $? -eq 0 ]; then
+echo '[INSTALL] Found pip'
+if [[ $unamestr == 'Darwin' ]]; then
+    python3 -m pip install --no-cache-dir --upgrade pip
+else
+    python3 -m pip install --no-cache-dir --upgrade pip --user
+fi
+else
+echo '[ERROR] python3-pip not installed'
+exit 1
+fi
+echo '[INSTALL] Using python virtualenv'
+rm -rf ./venv
+python3 -m venv ./venv
+if [ $? -eq 0 ]; then
+    echo '[INSTALL] Activating virtualenv'
+    source venv/bin/activate
+    pip install --upgrade pip wheel
+else
+    echo '[ERROR] Failed to create virtualenv'
+    exit 1
+fi
+echo '[INSTALL] Installing Requirements'
+pip install --no-cache-dir --use-deprecated=legacy-resolver -r requirements.txt
+npm install fast-xml-parser
+npm install -g serverless
+sls plugin install -n serverless-python-requirements && sls plugin install -n serverless-s3-deploy && sls plugin install -n serverless-wsgi
+sls deploy
